19483fb0c075e01678d71a9ee372e800d504023c
Rodrigo de Lima Cunha <rodrigo.cunha@youxgroup.com.br>
2023-05-29 21:32:52 -0300

    Aula 29/05/2023

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,58 @@ import { View, Text, StyleSheet } from "react-native";
 import Simples from "./componentes/Simples";
 import Frag from "./componentes/Frag";
 import ParImpar from "./componentes/ParImpar";
-
 import Familia from "./componentes/relacao/Familia";
 import Membro from "./componentes/relacao/Membro";
-
 import UsuarioLogado from "./componentes/UsuarioLogado";
-
 import Botao from "./componentes/Botao";
 import Contador from "./componentes/Contador";
 import DigiteSeuNome from "./componentes/DigiteSeuNome";
-
 import DimensoesFixas from "./componentes/DimensoesFixas";
-
 import Mega from "./componentes/MegaSena/Mega";
 
+import Button from "./componentes/Calculadora/Button";
+import Display from "./componentes/Calculadora/Display";
+
 export default class App extends Component{
+  state = {
+    displayValue: '0'
+  }
+
+  clearDisplay = () => {
+    this.setState({displayValue: '0'})
+  }
+  setDigit = (digito:any) =>{
+    this.setState({displayValue: digito})
+  }
+  setOperation = (op:any) =>{
+
+  }
   render() {
     return (
-      <View style={estilos.container}>
-        {/*<Simples texto='Rodrigo' /> 
+      <View style={estilos.calculadora}>
+        <Display value={this.state.displayValue} />
+        <View style={estilos.buttons}>
+          <Button label='AC' triple onClick={() => this.clearDisplay()} />
+          <Button label='/' op onClick={this.setOperation}/>
+          <Button label='7' onClick={this.setDigit}/>
+          <Button label='8' onClick={this.setDigit}/>
+          <Button label='9' onClick={this.setDigit}/>
+          <Button label='*' op onClick={this.setOperation}/>
+          <Button label='4' onClick={this.setDigit}/>
+          <Button label='5' onClick={this.setDigit}/>
+          <Button label='6' onClick={this.setDigit}/>
+          <Button label='-' op onClick={this.setOperation}/>
+          <Button label='1' onClick={this.setDigit}/>
+          <Button label='2' onClick={this.setDigit}/>
+          <Button label='3' onClick={this.setDigit}/>
+          <Button label='+' op onClick={this.setOperation}/>
+          <Button label='0' double onClick={this.setDigit}/>
+          <Button label='.' onClick={this.setDigit}/>
+          <Button label='=' op onClick={this.setOperation}/>
+        </View>
+
+         {/* <View style={estilos.container}>
+        <Simples texto='Rodrigo' /> 
          <Frag titulo="Cadastro" 
               subTitulo="Teda de Cadastro de Produto" /> 
          <ParImpar numero={9}  /> 8 
@@ -36,11 +69,11 @@ export default class App extends Component{
         <UsuarioLogado usuario={ null } /> 
         <Contador valorInicial={55} />         
         <DigiteSeuNome /> 
-        <DimensoesFixas /> */}
-
-        <Mega qtdeNumeros={7} />
-
-      </View>
+        <DimensoesFixas /> 
+        <Mega qtdeNumeros={7} /> 
+      </View> */}
+      </View>  
+     
     );      
   }
 }
@@ -51,8 +84,14 @@ export default class App extends Component{
       justifyContent: 'center',
       alignItems: 'center',
     },
-
     fonte: {
       fontSize: 50,
+    },
+    calculadora:{
+      flex: 1,
+    },
+    buttons:{
+      flexDirection: 'row',
+      flexWrap: 'wrap',
     },
   });
---
+++ src/componentes/Calculadora/Button.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { StyleSheet, Text, Dimensions,
+     TouchableHighlight } from "react-native";
+
+const estilo = StyleSheet.create({
+    button:{
+        fontSize: 41,
+        height: Dimensions.get('window').width / 4,
+        width: Dimensions.get('window').width /4,
+        padding: 20,
+        backgroundColor: '#f0f0f0',
+        textAlign: 'center',
+        borderWidth: 1,
+        borderColor: '#000'
+    },
+    buttonOp:{
+        color: '#fff',
+        backgroundColor: '#fa8231',
+    },
+    buttonDouble:{
+        width: (Dimensions.get('window').width / 4) * 2,
+    },
+    buttonTriple:{
+        width: (Dimensions.get('window').width / 4) * 3,
+    },
+})
+
+export default props => {
+    const styleButton = [estilo.button]
+    if (props.double) styleButton.push(estilo.buttonDouble)
+    if (props.triple) styleButton.push(estilo.buttonTriple)
+    if (props.op) styleButton.push(estilo.buttonOp)
+
+    return (
+        <TouchableHighlight onPress={() => props.onClick(props.label)}>
+            <Text style={styleButton}>{props.label}</Text>
+        </TouchableHighlight>
+    )
+}
